007d54b7dd390458473250c62e7d0e5f33067acb
Suzanne <shlindel@haverford.edu>
2023-04-28 13:20:34 -0400

    added line about github

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
 public class Main {
-    public static void main (String[] args){System.out.println("Testing new project");}
+    public static void main (String[] args){System.out.println("Testing new project");
+    System.out.println("Updated code and pushed to github");}
 }
